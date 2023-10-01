99017575c57fa079a2e230e33a909da6fd62062a
Joao Capucho <jhcapucho@gmail.com>
2023-10-01 12:15:19 +0200

    remove example config

--- lua/plugins/telescope-fzf-native.lua
+++ lua/plugins/telescope-fzf-native.lua
@@ -1,5 +1,37 @@
 return {
   "telescope.nvim",
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>fp",
+      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      desc = "Find Plugin File",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").git_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Git files",
+    },
+    {
+      "<leader>fws",
+      function()
+        require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+      end,
+      desc = "Find Git files",
+    },
+  },
+  -- change some options
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
