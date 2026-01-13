27cccf2f80ca2f728b2422b6cf114a5cb33490ce
Henrique Capucho <jhcapucho@gmail.com>
2026-01-13 11:09:54 +0100

    remove support to java and rust, add go support

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -7,7 +7,7 @@ return {
       opts = function(_, opts)
         opts.ensure_installed = opts.ensure_installed or {}
         table.insert(opts.ensure_installed, "js-debug-adapter")
-        table.insert(opts.ensure_installed, "java-debug-adapter")
+        table.insert(opts.ensure_installed, "delve")
         table.insert(opts.ensure_installed, "debugpy")
         table.insert(opts.ensure_installed, "cpptools")
       end,
@@ -31,15 +31,38 @@ return {
         },
       }
     end
-    dap.configurations.java = {
-      {
-        type = "java",
-        request = "attach",
-        name = "Debug (Attach) - Remote",
-        hostName = "127.0.0.1",
-        port = 5005,
+    -- Go DAP configuration
+    dap.adapters.go = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+        args = { "dap", "-l", "127.0.0.1:${port}" },
       },
     }
+
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "Debug test",
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "Debug Package",
+        request = "launch",
+        program = "${fileDirname}",
+      },
+    }
+
     -- TypeScript/JavaScript DAP configuration with ESM support
     for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
       if not dap.configurations[language] then
